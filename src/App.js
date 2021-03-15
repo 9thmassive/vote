b6ed95063ecd73fcb14e6044d3dd99006de8e44f
Arthur <62344195+9thmassive@users.noreply.github.com>
2021-03-15 22:43:25 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import Card from './card/Card'
 import './App.css'
-import firebase from 'firebase'
+
 import NavBar from './NavBar'
 
 function App() {
     let lang = ['JavaScript', 'Php', 'Ruby', 'C++', 'Python', 'Java', 'C#']
-    const handleLogin = () => {
-        const provider = new firebase.auth.GoogleAuthProvider()
-        firebase
-            .auth()
-            .signInWithPopup(provider)
-            .then((result) => {
-                alert('Success')
-            })
-            .catch((error) => {
-                alert(error.messages)
-            })
-    }
+    
 
     return (
         <>
-            <NavBar click={handleLogin} />
+            <NavBar  />
             <div className="App">
                 <h1 className="vote">Vote your Language</h1>
                 {lang.map((el) => (
